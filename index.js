f8ce9b74352ce6e86fa8ceffdfc4239ae4b885d6
sinead <7238289+nicfheorais@users.noreply.github.com>
2021-12-06 17:06:49 +0000

    M+S: saving after task 4 - installing and using chalk - note - not saving node_modules

--- index.js
+++ index.js
@@ -2,15 +2,21 @@
    mohamed: node v16.13.1; npm: 8.1.2  */
 
    import myCollection from "./collection.js";
+   import chalk from "chalk";
    
-   //import { myCollection } from "./collection.js"
-
 function describeItem(item){
     // fyi - can access item.name, item.count, item.whatILike
+    // fyi - console.log(chalk.blue('Hello world!'));
+
     if (item.count === 1) {
-        console.log(`I have a ${item.name}. Here's what I like about it: ${item.whatILike}`)
+        console.log(`I have a ${chalk.cyan(item.name)}. Here's what I like about it: ${chalk.green(item.whatILike)}`);
     } else {
-        console.log(`I have ${item.count} ${item.name}s. Here's what I like about them: ${item.whatILike}`)
+       // console.log(`I have ${item.count} ${chalk.cyan(item.name)}s. Here's what I like about them: ${item.whatILike}`);
+       console.log(`I have ${chalk.yellow(item.count)} ${chalk.cyan(item.name)}${chalk.cyan("s")}. Here's what I like about them: ${chalk.green(item.whatILike)}`);
+    
+
+       // oh wait - sudden thought - am i meant to not put the node-modules into git?
+       // for now - i'll just push index.js and package .json and i'll ask on slack.
     }
 }
    

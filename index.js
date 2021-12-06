32ab143a962f4458be7d90bae06f4eb3e4c9d7bb
sinead <7238289+nicfheorais@users.noreply.github.com>
2021-12-06 16:36:06 +0000

    m+s: saving after part 2 complete

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
 /* sinead: node v16.13.1; npm: 8.1.2
    mohamed: node v16.13.1; npm: 8.1.2  */
 
+let myCollection = [
+    {
+        name: "School of Code mug",
+        count: 1,
+        whatILike: "It has my cute pixel character on it!"
+    },
+    {
+        name: "School of Code hat",
+        count: 2,
+        whatILike: "An often overlooked fashion accessory"
+    }, 
+    {
+        name: "School of Code pillow",
+        count: 1,
+        whatILike: "Eat. Sleep. Code. Repeat :)"
+    }
+];
+
+function describeItem(item){
+    // fyi - can access item.name, item.count, item.whatILike
+    if (item.count === 1) {
+        console.log(`I have a ${item.name}. Here's what I like about it: ${item.whatILike}`)
+    } else {
+        console.log(`I have ${item.count} ${item.name}s. Here's what I like about them: ${item.whatILike}`)
+    }
+}
    
+function describeCollection (myArrayOfObjects){
+    //for (i=0; i < myArrayOfObjects.length; i++){
+    //   describeItem(myArrayOfObjects[i]);}
+    myArrayOfObjects.forEach(describeItem);
+}
+
+describeCollection(myCollection);
+
+//describeItem(myCollection[0]);
+//describeItem(myCollection[1]);
+//describeItem(myCollection[2]);
+
+//console.log(myCollection);
